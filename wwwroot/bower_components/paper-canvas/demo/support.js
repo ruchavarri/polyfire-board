978db2379572f65f46ce712f973819b1c70a2af8
dvdchavarri@gmail.com <Alvicius@192.168.1.34>
2016-03-24 12:51:22 +0100

    Delete Dependencies

--- wwwroot/bower_components/paper-canvas/demo/support.js
+++
@@ -1,8 +0,0 @@
-(function (window, undefined) {
-    var Canvas = !!document.createElement("canvas").getContext;
-
-    if (!Canvas) {
-        var supportWarning = document.getElementsByClassName('brower-support');
-        supportWarning[0].style.display = 'block';
-    }
-})(window);
